60e20e6125826ec01809452ef00d469a2f8f7229
net2katsu <net2katsu1507@yahoo.co.jp>
2021-07-23 03:06:41 +0000

    シバンなどの使い方

---
+++ test3/src/siban2.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace hello\test;
+
+var_dump("hello2");
+
+include __DIR__ . "/siban1.php";
+//include __FILE__;
+
+// class Test 
+// {
+//   function test()
+//   {
+//     var_dump(__);
+//   }
+// }
+
+// $test = new Test();
+///var_dump($test->test());
